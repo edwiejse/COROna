ab40255403816706b887f6cd969d2e9634954b1d
edwiejse <edwiejse07@gmail.com>
2020-04-04 21:43:18 +0530

    finalll

---
+++ assets/js/project.js
@@ -0,0 +1,9 @@
+var grid =document.querySelector(".grid");
+var body =document.querySelector("body");
+var transition =document.querySelector(".txt");
+// var wrap =document.querySelector(".txt");
+
+window.onload = function() {
+  body.classList.remove("preload");
+
+ };
